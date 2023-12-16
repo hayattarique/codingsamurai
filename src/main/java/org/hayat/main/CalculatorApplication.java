e2dfec6395fbb3bd2e23895d0570edaf7a3ed43c
hayat <hayat@esparksit.com>
2023-12-16 22:12:03 +0530

    calculator

---
+++ src/main/java/org/hayat/main/CalculatorApplication.java
@@ -0,0 +1,10 @@
+package org.hayat.main;
+
+import org.hayat.calculator.Calculator;
+
+public class CalculatorApplication {
+    public static void main(String[] args) {
+        new Calculator();
+    }
+
+}
